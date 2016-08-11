5bde888a5ba66fb78c05448b6e1e7726b2734d5b
yinhang2 <1095639697@qq.com>
2016-08-11 11:39:23 +0800

    2016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:392016/8/11 11:39

---
+++ Main3.java
@@ -0,0 +1,7 @@
+package august_eleven;
+
+public class Main3 {
+	public static void main(String args[]){
+		StaticBloc cBloc = new StaticBloc();
+	}
+}
---
+++ StaticBloc.java
@@ -0,0 +1,8 @@
+package august_eleven;
+
+public class StaticBloc {
+	static{
+		System.out.println("hello world!");
+		System.exit(1);
+	}
+}
